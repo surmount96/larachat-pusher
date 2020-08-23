f0ec22eabe8ac6d530916172279ba8a4f0fd84dd
surmount96 <ayanwoye74@gmail.com>
2020-08-23 23:40:24 +0100

    updates

--- app/User.php
+++ app/User.php
@@ -2,14 +2,16 @@
 
 namespace App;
 
+use App\Traits\Uuids;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Cache;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use HasRoles, Notifiable, Uuids;
 
     /**
      * The attributes that are mass assignable.
@@ -17,7 +19,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password'
+        'name', 'email', 'password','id'
     ];
 
     /**
@@ -36,10 +38,15 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'id' => 'string',
     ];
 
     protected $appends = ['isOnline'];
 
+    public function payments()
+    {
+        return $this->hasMany('App\Payment');
+    }
     public function chats()
     {
 //        return Message::whereIn('sender_id',$this->id)->whereIn('receiver_id',$this->id)->orderBy('created_at','asc')->get();

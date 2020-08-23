208b8541b1c5f1c181ec0642efccaf4c9ce6d2c1
surmount96 <ayanwoye74@gmail.com>
2020-08-23 23:34:20 +0100

    updates

--- app/User.php
+++ app/User.php
@@ -36,6 +36,7 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'id' => 'string'
     ];
 
     protected $appends = ['isOnline'];
